232d06d2053dcfb0091e9ceff7bdca188f125793
Cem Eke <cem.eke@pazarama.com>
2021-12-14 22:03:09 +0300

    UI Created

---
+++ armut/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  armut
+//
+//  Created by Cem Eke on 14.12.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
